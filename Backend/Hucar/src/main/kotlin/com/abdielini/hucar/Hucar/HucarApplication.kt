29e259ac496b601269ec0296fb8b44c2088ac946
abdiel <abdiel.escamilla.v@gmail.com>
2024-04-24 11:51:03 -0600

    Adding backend

---
+++ Backend/Hucar/src/main/kotlin/com/abdielini/hucar/Hucar/HucarApplication.kt
@@ -0,0 +1,11 @@
+package com.abdielini.hucar.Hucar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HucarApplication
+
+fun main(args: Array<String>) {
+	runApplication<HucarApplication>(*args)
+}
---
+++ Backend/Hucar/src/test/kotlin/com/abdielini/hucar/Hucar/HucarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abdielini.hucar.Hucar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HucarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
